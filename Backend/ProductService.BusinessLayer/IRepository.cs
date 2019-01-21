502924a7c1b251044e5f56e889e083bbf3d94482
Niels Boerkamp <n.boerkamp@outlook.com>
2019-01-21 21:06:54 +0100

    Added an interface to specify the functions of the GenericRepository

---
+++ Backend/ProductService.BusinessLayer/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProductService.BusinessLayer
+{
+	public interface IRepository
+	{ }
+
+	public interface IRepository<T> where T : Entity
+	{
+		Task<IList<T>> GetAll();
+		Task<T> Add(T entity);
+		Task<T> Update(T entity);
+		Task<bool> Delete(T entity);
+	}
+}
