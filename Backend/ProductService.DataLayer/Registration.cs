d6624960b94b86c6df79970e553698404c6c4118
Niels Boerkamp <n.boerkamp@outlook.com>
2019-01-22 22:00:33 +0100

    Added dependency registration

---
+++ Backend/ProductService.DataLayer/Registration.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductService.BusinessLayer;
+using ProductService.DataLayer.Context;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace ProductService.DataLayer
+{
+	public static class Registration
+	{
+		private static IConfigurationRoot configuration;
+
+		static Registration()
+		{
+			configuration = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+				.Build();
+		}
+
+		/// <summary>
+		/// This function adds all the services we need for the DataLayer.
+		/// </summary>
+		/// <returns>The data layer.</returns>
+		/// <param name="services">Services.</param>
+		public static IServiceCollection AddDataLayer(this IServiceCollection services)
+		{
+			services.AddDatabaseContext();
+			services.AddRepositories();
+			services.AddDatabaseHealthCheck();
+
+			return services;
+		}
+
+		/// <summary>
+		/// Add the database context so we can use it with Dependency Injection.
+		/// </summary>
+		/// <returns>The database context.</returns>
+		/// <param name="services">Services.</param>
+		private static IServiceCollection AddDatabaseContext(this IServiceCollection services)
+		{
+			// Add the database context as a singleton.
+			services.AddDbContext<ProductServiceContext>(options =>
+				options.UseNpgsql(configuration.GetConnectionString("ConnectionString")), ServiceLifetime.Scoped);
+
+			return services;
+		}
+
+		/// <summary>
+		/// Find all repositories in the assembly and register them as scoped
+		/// dependencies.
+		/// </summary>
+		/// <returns>The repositories.</returns>
+		/// <param name="services">Services.</param>
+		private static IServiceCollection AddRepositories(this IServiceCollection services)
+		{
+			// Get all the repository types.
+			var repositories = Assembly
+						.GetCallingAssembly()
+						.GetTypes()
+						.Where(x => x.IsClass && !x.IsAbstract)
+						.Where(x => typeof(IRepository).IsAssignableFrom(x));
+
+			foreach (Type repository in repositories)
+			{
+				// Find the corresponding interface type.
+				var repoInterfaces = repository.GetInterfaces()
+					.Where(x => typeof(IRepository).IsAssignableFrom(x))
+					.Where(x => x.Name != typeof(IRepository).Name)
+					.Where(x => !x.IsGenericType);
+
+				// There should only be one repository interface implemented by this repo. 
+				//`Single` will throw an exception if there are implemented more
+				// than one.
+				var repoInterface = repoInterfaces.Single();
+
+				// Register the repository and its interface to the service.
+				services.AddScoped(repoInterface, repository);
+			}
+
+			return services;
+		}
+
+		private static IServiceCollection AddDatabaseHealthCheck(this IServiceCollection services)
+		{
+			services.AddHealthChecks()
+				.AddDbContextCheck<ProductServiceContext>();
+
+			return services;
+		}
+	}
+}
--- Backend/ProductService/Startup.cs
+++ Backend/ProductService/Startup.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using ProductService.DataLayer;
 
 namespace ProductService
 {
@@ -15,6 +12,8 @@ namespace ProductService
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			// Register the DataLayer, among other the repositories and database healthcheck.
+			services.AddDataLayer();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
